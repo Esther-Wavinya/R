5b64f6ee2f8d94d948cf8e9ba386fd7a76aded5c
Esther-Wavinya <estherwavinya80@gmail..com>
2023-03-16 21:04:05 +0300

    main r

--- main.R
+++ main.R
@@ -165,3 +165,33 @@ stars_wars_matrix <- matrix(box_office, nrow = 3, byrow = TRUE)
 stars_wars_matrix
 
 
+new_hope <- c(460.998, 314.4)
+empire_strikes <- c(290.475, 247.900)
+return_jedi <- c(309.306, 165.8)
+star_wars_matrix <- matrix(c(new_hope, empire_strikes, return_jedi))
+region <- c("US", "non-US")
+titles <- c("A New Hope", "The Empire Strikes Back", "Return of the Jedi")
+colnames(star_wars_matrix) <- region
+rownames(star_wars_matrix) <- titles
+star_wars_matrix
+
+# Box office Star Wars (in millions!)
+new_hope <- c(460.998, 314.4)
+empire_strikes <- c(290.475, 247.900)
+return_jedi <- c(309.306, 165.8)
+
+# Construct matrix
+star_wars_matrix <- matrix(c(new_hope, empire_strikes, return_jedi), nrow = 3, byrow = TRUE)
+
+# Vectors region and titles, used for naming
+region <- c("US", "non-US")
+titles <- c("A New Hope", "The Empire Strikes Back", "Return of the Jedi")
+
+# Name the columns with region
+colnames(star_wars_matrix) <- region
+
+# Name the rows with titles
+rownames(star_wars_matrix) <- titles
+
+# Print out star_wars_matrix
+star_wars_matrix
